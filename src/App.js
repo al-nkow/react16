57743ad2f8613f5d85bfe315cffb4852a1013546
Aleksey <akovalchuk@bekitzur.com>
2018-02-16 19:12:01 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import './App.css';
+import classes from './App.css';
 
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -49,56 +50,37 @@ class App extends Component {
     };
 
     render() {
-        const myStyles = {
-            backgroundColor: '#e09b94',
-            fontSize: '16px',
-            fontFamily: 'Arial',
-            padding: '10px',
-            borderColor: '#c3776e',
-            borderRadius: '4px',
-            color: '#333333',
-            cursor: 'pointer',
-            marginRight: '20px',
-            outline: 'none',
-        };
-        const newBlockStyle = {
-            border: '2px solid #597089',
-            color: '#597089',
-            width: '200px',
-            height: '100px',
-            padding: '10px',
-            margin: '20px auto',
-        };
 
         let persons = null;
+        let btnClass = '';
+
+
         if (this.state.showPersons) {
             persons = (<div>
                 {this.state.persons.map((person, index) => {
-                    return <Person
+                    return <ErrorBoundary key={person.id}><Person
                         myClick={() => this.deletePersonHandler(index)}
                         name={person.name}
                         age={person.age}
-                        key={person.id}
-                        changed={(event) => this.nameChangeHandler(event, person.id)}/>
+                        // key={person.id}
+                        changed={(event) => this.nameChangeHandler(event, person.id)}/></ErrorBoundary>
                 })}
             </div>);
-            myStyles.backgroundColor = '#1c5093';
-            myStyles.color = '#ffffff';
-            myStyles.borderColor = '#243e61';
+            btnClass = classes.Red;
         }
 
-        let classes = [];
-        if (this.state.persons.length <= 2) classes.push('red');
-        if (this.state.persons.length <= 1) classes.push('bold');
+        let assignedClasses = [];
+        if (this.state.persons.length <= 2) assignedClasses.push( classes.red );
+        if (this.state.persons.length <= 1) assignedClasses.push( classes.bold );
 
         return (
-            <div className="App">
+            <div className={classes.App}>
                 <h1>Hello world</h1>
-                <p className={classes.join(' ')}>This is really working</p>
+                <p className={assignedClasses.join(' ')}>This is really working</p>
 
-                <button key='q2' style={myStyles} onClick={() => this.switchNameHandler('Petya')}>Switch name</button>
+                <button className={btnClass} key='q2' onClick={() => this.switchNameHandler('Petya')}>Switch name</button>
 
-                <button key='q1' style={myStyles} onClick={this.togglePersonsHandler}> SHOW/HIDE</button>
+                <button className={btnClass} key='q1' onClick={this.togglePersonsHandler}> SHOW/HIDE</button>
 
                 {persons}
 
